0189dd6dbf94c5237a19bcff0de0b34b64174694
Bob Hammell <rhammell@gmail.com>
2025-03-25 13:31:29 -0400

    Refactor icon definitions

---
+++ Arduino/grid_bot/icons.h
@@ -0,0 +1,10 @@
+#ifndef ICONS_H
+#define ICONS_H
+
+#include <Arduino.h>
+
+// Icon data declarations
+extern const uint16_t UNDO_ICON_COLOR[] PROGMEM;
+extern const uint16_t SETTINGS_ICON_COLOR[] PROGMEM;
+
+#endif
